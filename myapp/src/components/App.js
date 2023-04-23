d8865f0e08094d62c4bf3652bba17506aa53312a
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-04-23 16:25:38 +0530

    event in react js

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,42 +1,13 @@
-import { Component } from "react";
-import Table from './Table'
+import { Component } from "react"
 
 class App extends Component {
-    state = {
-        charachters: [
-            {
-                Name: "Deva Krishnan",
-                Rollno: "22eca18"
-            },
-            {
-                Name: "John Wesly",
-                Rollno: "22eca49"
-            },
-            {
-                Name: "Prathap",
-                Rollno: "22eca23"
-            },
-            {
-                Name: "Arumugam",
-                Rollno: "22eca98"
-            }
-        ]
-    }
-
-    removeCharachter = (index) => {
-        const { charachters } = this.state;
-        let filtered = charachters.filter((charachter, i) => {
-            return i !== index
-        });
-
-        this.setState({charachters:filtered})
-    }
-
     render() {
-        const { charachters } = this.state
+        let myFunction = (Name, event) => {
+            alert (Name + event.type);
+        }
 
         return (
-            <Table removeCharachter = { this.removeCharachter } charachtersData = { charachters }/>
+            <button onClick={ (event) => myFunction("Deva Krishnan", event) }>button</button>
         )
     }
 }
