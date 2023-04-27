c84509ec509e12985826a7fa043c5ed18d4eb9f0
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-04-27 13:26:43 +0530

    useMemo hook was completed

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,24 +1,27 @@
-import React, { useReducer } from "react";
+import React, { useMemo, useState } from "react";
 
-function Counter(state, action) {
-  switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    default:
-      alert("Invalid action");
-  }
+function Memocomponent(props) {
+  const {a,b} = props;
+  const result = useMemo(() => {
+    return a+b;
+  }, [a, b])
+  return <h1>{a} + {b} = {result}</h1>
 }
 
 export default function App() {
-  const [Count, dispatch] = useReducer(Counter, 0);
-  
+  const [Count, setCount] = useState(0);
+
+  const a = Count;
+  const b = Count * 2;
+
+
   return (
     <>
-      <h1>The Counter is: {Count}</h1>
-      <button onClick={() => dispatch({type: "increment"})}>increment</button>
-      <button onClick={() => dispatch({type: "decrement"})}>decrement</button>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <h1>The Count is: { Count }</h1>
+      <button onClick={() => setCount(Count + 1)}>Increment</button>
+      <Memocomponent a = {a} b = {b}/>
     </>
   );
 }
